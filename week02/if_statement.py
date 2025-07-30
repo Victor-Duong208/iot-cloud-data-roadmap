e52fbb75e5e208b01c4d56899d4bbea1d5367071
Duong Nhat Vinh <dnvinh0802@gmail.com>
2025-07-30 22:51:47 +0700

    feat: luyện tập cấu trúc điều kiện if/else/elif

---
+++ week02/if_statement.py
@@ -0,0 +1,10 @@
+score = float(input("Nhập điểm: "))
+
+if score >= 9.0:
+    print("🎉 Xuất sắc")
+elif score >= 7.0:
+    print("👍 Khá")
+elif score >= 5.0:
+    print("🙂 Trung bình")
+else:
+    print("❌ Yếu")
