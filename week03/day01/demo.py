b078ff0d9721f25d48b939671823719e018ab3e3
Duong Nhat Vinh <dnvinh0802@gmail.com>
2025-08-10 23:37:19 +0700

    w03-d01: add moving_average & normalize + demo

--- week03/day01/demo.py
+++ week03/day01/demo.py
@@ -1,7 +1,10 @@
-from src.utils_math import mean, to_celsius, categorize_temp
+from src.utils_math import mean, to_celsius, categorize_temp, moving_average, normalize
 
 temps_f = [77, 80.6, 86, 95]
 temps_c = [to_celsius(t) for t in temps_f]
 print("Celsius:", [round(t, 2) for t in temps_c])
 print("Mean C:", round(mean(temps_c), 2))
 print("Tag:", categorize_temp(mean(temps_c)))
+
+print("MA(3):", [round(x,2) for x in moving_average(temps_c, 3)])
+print("Norm:", [round(x,2) for x in normalize(temps_c)])
